27d5bfc8ba7ce122625dfb3d94a72a4db18c8b89
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2019-06-30 22:30:33 -0300

    Initial Commit

---
+++ src/Element/BoletoUSP.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\WebformBoletoUsp\Element;
+
+use Drupal\Core\Render\Element;
+use Drupal\Core\Render\Element\FormElement;
+use Drupal\Core\Form\FormStateInterface;
+
+class BoletoUSP extends FormElement {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getInfo() {
+    return parent::getInfo();
+  }
+}
---
+++ src/Plugin/WebformElement/WebformBoletoUSP.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\WebformBoletoUsp\Plugin\WebformElement;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\webform\Plugin\WebformElementBase;
+use Drupal\webform\WebformSubmissionInterface;
+
+class WebformBoletoUSP extends WebformElementBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultProperties() {
+    return parent::getDefaultProperties();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepare(array &$element, WebformSubmissionInterface $webform_submission = NULL) {
+    parent::prepare($element, $webform_submission);
+
+    // Here you can customize the webform element's properties.
+    // You can also customize the form/render element's properties via the
+    // FormElement.
+    //
+    // @see \Drupal\webform_example_element\Element\WebformExampleElement::processWebformElementExample
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public function form(array $form, FormStateInterface $form_state) {
+    $form = parent::form($form, $form_state);
+    // Here you can define and alter a webform element's properties UI.
+    // Form element property visibility and default values are defined via
+    // ::getDefaultProperties.
+    //
+    // @see \Drupal\webform\Plugin\WebformElementBase::form
+    // @see \Drupal\webform\Plugin\WebformElement\TextBase::form
+    return $form;
+  }
+
+
+
+
+
+}
