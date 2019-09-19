cb24c428e6ed4fb1ac87074cc1377dce22193374
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2019-09-19 18:20:50 -0300

    wip

--- src/Plugin/WebformElement/WebformElementBoletoUSP.php
+++ src/Plugin/WebformElement/WebformElementBoletoUSP.php
@@ -14,7 +14,7 @@ use Drupal\webform_boleto_usp\Gera;
  *   id = "boletousp",
  *   default_key = "boletousp",
  *   api = "https://github.com/fflch/webform-boleto-usp",
- *   label = @Translation("BOLETOUSP"),
+ *   label = @Translation("Boleto USP"),
  *   description = @Translation("Provides a form element that manage boleto USP"),
  *   category = @Translation("Advanced elements"),
  *   states_wrapper = TRUE,
@@ -34,8 +34,8 @@ class WebformElementBoletoUSP extends WebformElementBase {
       # Campos do boleto
       'boletousp_informacoesboletosacado' => 'Nome do evento, curso etc',
       'boletousp_datavencimentoboleto' =>'', 
-      'boletousp_valor' => '10,00',
-      'cpf' => '',
+      'boletousp_valor' => '10',
+      'boletousp_cpf' => '',
     ];
   }
 
@@ -74,12 +74,11 @@ class WebformElementBoletoUSP extends WebformElementBase {
     parent::prepare($element, $webform_submission);
 
     /* Elements para mapeamento CPF e email */
-    //
     $elements = $webform_submission->getWebform()->getElementsDecodedAndFlattened();
     foreach($elements as $key=>$element){
         $this->$elements[$key] = $element['#title'];
     }
-    //echo "<pre>"; print_r(parent::getCompositeElements());die();
+    
   }
 
   /**
@@ -106,10 +105,6 @@ class WebformElementBoletoUSP extends WebformElementBase {
    * {@inheritdoc}
    */
   public function form(array $form, FormStateInterface $form_state) {
-    die();
-    // public function getElementStateOptions() {
-    // $this->getCompositeElements();
-
     $form = parent::form($form, $form_state);
 
     $boletousp_types = ['default' => $this->t('Default challenge type')];
@@ -122,6 +117,7 @@ class WebformElementBoletoUSP extends WebformElementBase {
     $form['boletousp']['boletousp_container'] = [
       '#type' => 'container',
     ];
+
     $form['boletousp']['boletousp_container']['boletousp_informacoesboletosacado'] = [
       '#type' => 'textfield',
       '#attributes'  => ['size' => 125],
@@ -139,13 +135,20 @@ class WebformElementBoletoUSP extends WebformElementBase {
       '#type' => 'date',
       '#title' => $this->t('Data de vencimento do boleto'),
     ];
-/*
-    $form['boletousp']['boletousp_container']['cpf'] = [
-      '#type'        => 'select',
-      '#title'       => $this->t('Cpf'),
-      '#options' => $this->$elements,
+
+    $form['boletousp']['boletousp_container']['mapeamento'] = [
+      '#type' => 'fieldset',
+      '#description' => $this->t("sss"),
+      '#title' => $this->t('Mapeamento com campos do formulário'),
     ];
-*/
+
+    $form['boletousp']['boletousp_container']['mapeamento']['boletousp_cpf'] = [
+      '#type' => 'textfield',
+      '#description' => $this->t("sss"),
+      '#attributes'  => ['size' => 20],
+      '#title' => $this->t('CPF'),
+    ];
+
     return $form;
   }
 
