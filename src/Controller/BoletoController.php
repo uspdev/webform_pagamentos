f46c3459fd3138b75a859acb5ddde80c886cd7d1
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2019-11-18 11:39:53 -0300

    pdf sendo gerado com sucesso

---
+++ src/Controller/BoletoController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\webform_boleto_usp\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\webform\Entity\WebformSubmission;
+use Uspdev\Boleto;
+
+/**
+ * Class BoletoController.
+ */
+class BoletoController extends ControllerBase {
+
+  public function gera($webform_submission_id) {
+    $webform_submission = WebformSubmission::load($webform_submission_id);
+
+    if($webform_submission) {
+
+      /* Verificamos se há boleto gerado para esse webform_submission */
+      $data = $webform_submission->getData();
+      if(isset($data['id_boleto']) && !empty($data['id_boleto'])){
+        
+        $boleto = new Boleto('fflch','');
+        $boleto->obter($data['id_boleto']);
+
+      }
+    }
+
+    return [
+      '#type' => 'markup',
+      '#markup' => $this->t('Não foi possível gerar o boleto.'),
+    ];
+  }
+
+}
